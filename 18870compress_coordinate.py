4d4542380ad8da7f4a1d7d581883fbb2d96d415e
irene622 <dywjd7272@naver.com>
2022-08-25 21:24:14 +0900

    1000 999 1000 999 1000 = [2, 0, 2, 0, 2]

--- 18870compress_coordinate.py
+++ 18870compress_coordinate.py
@@ -1,5 +1,5 @@
 num_num = int(input())
-num_list = map(input().split(), int)
+num_list = list(map(int, input().split()))
 
 [3,2,1,3,2,1]
 [2,1,0,2,1,0]
@@ -10,11 +10,15 @@ num_list = map(input().split(), int)
 def comp_coordinates(num_num, num_list) :
     result = [0] * num_num
     for i in range(num_num) :
-        for side_idx in range(i, num_num) :
+        for side_idx in range(i+1, num_num) :
             if num_list[i] < num_list[side_idx] :
                 result[side_idx] += 1
+            elif num_list[i] == num_list[side_idx] :
+                continue
             else :
-                result[i] = len(set(num_list)) - 1
+                result[i] += 1
+        # else :
+        #     result[i] = len(set(num_list)) - 1
     return result
 
 print(comp_coordinates(num_num, num_list))
