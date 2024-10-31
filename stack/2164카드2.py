4ac708e884f72ab03c8ac23c536e79917d054b59
irene622 <dywjd7272@naver.com>
2024-10-31 20:40:22 +0900

    queue. deque lib

---
+++ stack/2164카드2.py
@@ -0,0 +1,10 @@
+import sys
+from collections import deque
+
+queue = deque()
+queue.extend(range(1, int(sys.stdin.readline()) + 1))
+while len(queue) != 1:
+    queue.popleft()
+    queue.rotate(-1)
+print(queue[0])
+    
