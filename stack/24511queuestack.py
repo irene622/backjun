8addebedda0bf740e9a74110139265a46a525389
irene622 <dywjd7272@naver.com>
2024-11-01 12:11:47 +0900

    keep queue

--- stack/24511queuestack.py
+++ stack/24511queuestack.py
@@ -1,24 +1,19 @@
 import sys
 from collections import deque
 
+# Here are queue, queue = [[1],[4]]. Add 2.
+# Then, [[1,2],[4]]->[[2],[4,1]]->[[2],[1]] and return 4
+# [[1],[4]] -> [[2],[1],[4]] -> [[2],[1],[4]].pop()
 num_data_structure = int(sys.stdin.readline())
 data_structure = sys.stdin.readline().strip().split(' ')
-data_list = []
-for element in sys.stdin.readline().strip().split(' '):
-    one_deque = deque()
-    one_deque.append(element)
-    data_list.append(one_deque)
+data = sys.stdin.readline().strip().split(' ')
+queue = deque()
+for i in range(num_data_structure):
+    if data_structure[i] == '0':
+        queue.append(data[i])
 
 num_adding = int(sys.stdin.readline())
 adding_elem_list = sys.stdin.readline().strip().split(' ')
-result = ''
-for adding in adding_elem_list:
-    for i in range(num_data_structure):
-        if data_structure[i] == '0': # queue
-            data_list[i].append(adding)
-            adding = data_list[i].popleft()
-        elif data_structure[i] == '1': # stack
-            data_list[i].append(adding)
-            adding = data_list[i].pop()
-    result += adding + ' '
-print(result.strip())
+for element in adding_elem_list:
+    queue.appendleft(element)
+    print(queue.pop(), end=' ')
