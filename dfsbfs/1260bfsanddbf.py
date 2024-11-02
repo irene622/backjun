12b111fae77f3b8a3630a91d84eacda76b73331c
irene622 <dywjd7272@naver.com>
2024-11-03 01:45:26 +0900

    stack is dfs, queue is bfs

---
+++ dfsbfs/1260bfsanddbf.py
@@ -0,0 +1,68 @@
+import sys
+from collections import deque
+
+sys.stdin = open('dfsbfs/1260input.txt', 'r')
+num, lines, starter = map(int, sys.stdin.readline().strip().split(' '))
+graph = {i: [] for i in range(1, num + 1)}
+dfsvisited = {i: False for i in range(1, num + 1)}
+for _ in range(lines):
+    a, b = map(int, sys.stdin.readline().strip().split(' '))
+    if graph.get(a) == None:
+        graph[a] = [b]
+    else:
+        graph[a] += [b]
+
+    if graph.get(b) == None:
+        graph[b] = [a]
+    else:
+        graph[b] += [a]
+
+bfsvisited = {k:v for k,v in dfsvisited.items()}
+
+def dfs(starter):
+    stack = [starter]  # 스택 사용
+    dfsvisited[starter] = True
+    dfs_path = str(starter)
+
+    while stack:
+        current = stack.pop()  
+        for i in sorted(graph[current], reverse=True):  
+            if not dfsvisited[i]:
+                stack.append(i)
+                if not dfsvisited[current]:
+                    dfs_path += ' ' + str(current)
+                dfsvisited[current] = True
+    return dfs_path
+
+# def dfs(starter):
+#     dfsvisited[starter] = True
+#     bfs_path = str(starter)
+#     print(str(starter), end=' ')
+#     for neighbor in sorted(graph[starter]):
+#         if not dfsvisited[neighbor]:
+#             dfs(neighbor)
+#     # return bfs_path
+
+print(dfs(starter))
+# dfs(starter)
+# print('')
+
+def bfs(starter):
+    queue = deque([starter])
+    bfsvisited[starter] = True
+    bfs_path = str(starter)
+
+    while queue:
+        current = queue.popleft()
+        for i in sorted(graph[current]):
+            if not bfsvisited[i]:
+                queue.append(i)
+                bfsvisited[i] = True
+                bfs_path += ' ' + str(i)
+    return bfs_path
+
+print(bfs(starter))
+
+
+
+
