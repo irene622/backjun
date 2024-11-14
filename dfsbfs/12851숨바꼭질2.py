f81276fed21b2a7947a9840d7bec3092e73ca164
irene622 <dywjd7272@naver.com>
2024-11-15 00:55:14 +0900

    add comments

--- dfsbfs/12851숨바꼭질2.py
+++ dfsbfs/12851숨바꼭질2.py
@@ -19,9 +19,14 @@ def bfs(status, goal):
 
         directions = [current-1, current+1, current*2]
         for i in directions:
-            if 0<= i <= 100_000 and (visited[i] == 0 or visited[i]== visited[current] +1): #(visited[i] == 0 or visited[i]== visited[current] +1)?
+            # 범위에 있는지.
+            # 빠른 경로로 온 경우에 체크. 1-2-3-2는 반복하기에 제외.. 5-4-3-6는 5-6이 더 빠른 경로이기 때문에 5-4-3-6은 제외.
+            # visited[i] == visited[current] + 1. current의 depth와 i의 depth를 생각. 
+            #     i의 depth에서 조사하고자 하는 i가 중복되도 조사한다. 3-4-5와 3-6-5모두 조사해야 다른 경로임을 카운트할 수 있다.
+            if 0<= i <= 100_000 and (visited[i] == 0 or visited[i] == visited[current] + 1):
                 visited[i] = visited[current] + 1
                 next.append(i)
+
     print(sec)
     print(num_path)
     return num_path, sec
