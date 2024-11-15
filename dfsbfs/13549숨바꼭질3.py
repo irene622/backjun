9b30879ba2543496d5f113b8c40c599f319f6701
irene622 <dywjd7272@naver.com>
2024-11-15 17:38:20 +0900

    comments

--- dfsbfs/13549숨바꼭질3.py
+++ dfsbfs/13549숨바꼭질3.py
@@ -2,7 +2,7 @@ import sys
 from collections import deque
 
 N, K = map(int, sys.stdin.readline().strip().split(' '))
-visited = [0 for _ in range(100_001)]
+visited = [0 for _ in range(100_001)] # 방문을 확인할 것이면, visited = [False] * 100_001해도 된다.
 time = [0 for _ in range(100_001)]
 
 def checktime(start, goal):
@@ -17,14 +17,14 @@ def checktime(start, goal):
             break
 
         for i in [current-1, current+1, current*2] :
-            if 0<= i <= 100_000 and visited[i] == 0:
+            if 0<= i <= 100_000 and visited[i] == 0: # visited[i] == visited[current]+1 삭제.. 넣어도 상관없지 않나..?
                 visited[i] = visited[current] + 1
                 if i != current*2:
                     time[i] = time[current] + 1
                     next.append(i)
                 else:
                     time[i] = time[current]
-                    next.appendleft(i)
+                    next.appendleft(i) # append 아니고 appendleft.
                 
     
     print(shortest_time)
