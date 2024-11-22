a6923489983aca81a3501a083756fd8a3abec22b
irene622 <dywjd7272@naver.com>
2024-11-23 01:24:21 +0900

    change recording way. But same..

--- string/20437문자열게임.py
+++ string/20437문자열게임.py
@@ -4,22 +4,24 @@ game = int(sys.stdin.readline().strip())
 
 alphabet = 'abcdefghijklnmopqrstuvwxyz'
 record = {}
-for char in alphabet:
-    record[char] = {'count': 0, 'where': []}
+count_record = {}
+where_record = {}
+# for char in alphabet:
+#     record[char] = {'count': 0, 'where': []}
 
 for _ in range(game):
     line = sys.stdin.readline().strip()
     count = int(sys.stdin.readline().strip())
 
     for idx, char in enumerate(line):
-        record[char]['count'] += 1
-        record[char]['where'].append(idx)
+        count_record[char] = count_record.get(char, 0) + 1
+        where_record[char] = where_record.get(char, []) + [idx]
 
     cand_lenght = []
-    for char, info in record.items():
-        if info['count'] >= count:
-            for i in range(0, len(info['where'])-count+1):
-                length = record[char]['where'][i+count-1] - record[char]['where'][i]
+    for char, v in count_record.items():
+        if v >= count:
+            for i in range(0, len(where_record[char])-count+1):
+                length = where_record[char][i+count-1] - where_record[char][i]
                 cand_lenght.append(length)
     
     # print result
@@ -29,9 +31,8 @@ for _ in range(game):
         print(f'{min(cand_lenght)+1} {max(cand_lenght)+1}')
 
     # initialize
-    record = {}
-    for char in alphabet:
-        record[char] = {'count': 0, 'where': []}
+    count_record = {}
+    where_record = {}
 
     
     
