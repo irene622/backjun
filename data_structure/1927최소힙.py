ea62fe50ca1765fd0a42f4b449a6d1d8b8ee9a43
irene622 <dywjd7272@naver.com>
2024-11-30 23:13:41 +0900

    heap push

--- data_structure/1927최소힙.py
+++ data_structure/1927최소힙.py
@@ -2,27 +2,23 @@ import sys
 from collections import deque
 
 N = int(sys.stdin.readline().strip())
-result = deque([]) #(num, parent index)
+heap = [] # append number. its index is parents order
 for i in range(N):
     n = int(sys.stdin.readline().strip())
     if n == 0:
-        if len(result) == 0:
+        if len(heap) == 0:
             print(0)
         else:
-            print(result.popleft()[0], "h")
-    elif len(result) == 0:
-        result.append([n,-1])
+            print(heap[0], "h")
     else:
-        idx_parents = (i-1)//2
-
-        for data in result:
-            if data[1] == idx_parents:
-                parent = result[idx_parents]
-        if parent[0] > n:
-            result.append([n, parent[1]])
-            parent[1] = (i-1)//2
-        else:
-            result.append([n, idx_parents])
-
-
-print(result)
+        # heap push
+        heap.append(n)
+        current = len(heap) - 1
+        while current > 0 :
+            parent = (current - 1)//2
+            if heap[parent] > heap[current]:
+                heap[parent], heap[current] = heap[current], heap[parent]
+                current = parent
+            else:
+                break
+    print(heap)
