477afdd773041f9eee2dc8578fe08605872c9396
irene622 <dywjd7272@naver.com>
2024-12-06 23:24:18 +0900

    chore: format

--- data_structure/1927최소힙.py
+++ data_structure/1927최소힙.py
@@ -10,7 +10,6 @@ class Heap:
         # move_up = True이면 바꾼다.
         # [None,1,3,5,2]를 idx를 이용해서 [None,1,2,5,3]으로 바꾼다.
 
-
     def pop(self):
         if len(self.heap) <= 1:
             return 0
@@ -23,48 +22,56 @@ class Heap:
 
             idx = 1
             while self.move_down(idx):
-                left_child_idx = idx*2
-                right_child_idx = idx*2+1
+                left_child_idx = idx * 2
+                right_child_idx = idx * 2 + 1
 
                 # idx의 오른쪽 자식이 없을 때
                 if right_child_idx >= len(self.heap):
                     if self.heap[idx] > self.heap[left_child_idx]:
-                        self.heap[idx], self.heap[left_child_idx] = self.heap[left_child_idx], self.heap[idx]
+                        self.heap[idx], self.heap[left_child_idx] = (
+                            self.heap[left_child_idx],
+                            self.heap[idx],
+                        )
                         idx = left_child_idx
                 else:
                     # TODO...
                     if self.heap[left_child_idx] < self.heap[right_child_idx]:
                         if self.heap[idx] > self.heap[left_child_idx]:
-                            self.heap[idx], self.heap[left_child_idx] = self.heap[left_child_idx], self.heap[idx]
+                            self.heap[idx], self.heap[left_child_idx] = (
+                                self.heap[left_child_idx],
+                                self.heap[idx],
+                            )
                             idx = left_child_idx
                     else:
                         if self.heap[idx] > self.heap[right_child_idx]:
-                            self.heap[idx], self.heap[right_child_idx] = self.heap[right_child_idx], self.heap[idx]
+                            self.heap[idx], self.heap[right_child_idx] = (
+                                self.heap[right_child_idx],
+                                self.heap[idx],
+                            )
                             idx = right_child_idx
 
             return output
-
 
     def move_down(self, idx):
         # Compare current idx and its child idx.
         left_child_idx = idx * 2
         right_child_idx = idx * 2 + 1
-        
+
         # idx의 왼쪽 자식이 없을 때.
-        # idx=2의 왼쪽 자식은 idx 4임. 
+        # idx=2의 왼쪽 자식은 idx 4임.
         # idx 4가 존재하면 len(self.heap)이 5이상임. 아니면 len(self.heap)이 4이하임.
         if left_child_idx >= len(self.heap):
             return False
-        
+
         # idx의 오른쪽 자식이 없을 때.
-        # idx 2의 오른쪽 자식은 idx 5임. 
+        # idx 2의 오른쪽 자식은 idx 5임.
         # idx 5가 존재하면 len(self.heap)이 6이상임. 아니면 len(self.heap)이 5이하임.
         elif right_child_idx >= len(self.heap):
             if self.heap[idx] > self.heap[left_child_idx]:
                 return True
             else:
                 return False
-            
+
         else:
             # idx 2가 왼쪽자식보다 크면 바꾼다.
             if self.heap[idx] > self.heap[left_child_idx]:
@@ -74,19 +81,17 @@ class Heap:
                 return True
             else:
                 return False
-      
 
     def move_up(self, idx):
         # Compare current idx and its parents idx.
         if idx <= 1:
             return False
-        
+
         parent_idx = idx // 2
         if self.heap[idx] < self.heap[parent_idx]:
             return True
         else:
             return False
-
 
     def insert(self, num):
         self.heap.append(num)
@@ -94,12 +99,15 @@ class Heap:
         idx = len(self.heap) - 1
         while self.move_up(idx):
             parent_idx = idx // 2
-            self.heap[idx], self.heap[parent_idx] = self.heap[parent_idx], self.heap[idx]
+            self.heap[idx], self.heap[parent_idx] = (
+                self.heap[parent_idx],
+                self.heap[idx],
+            )
             idx = parent_idx
 
 
 h = Heap()
-sys.stdin = open('data_structure/ex.txt')
+sys.stdin = open("data_structure/ex.txt")
 N = int(sys.stdin.readline())
 for _ in range(N):
     num = int(sys.stdin.readline())
@@ -107,9 +115,6 @@ for _ in range(N):
         print(h.pop())
     else:
         h.insert(num)
-
-
-
 
 
 ### copy from https://developerbee.tistory.com/76
--- data_structure/2910빈도정렬.py
+++ data_structure/2910빈도정렬.py
@@ -1,17 +1,17 @@
 import sys
 
-N, C = map(int, sys.stdin.readline().strip().split(' '))
+N, C = map(int, sys.stdin.readline().strip().split(" "))
 info = {}
-seq = sys.stdin.readline().strip().split(' ')
+seq = sys.stdin.readline().strip().split(" ")
 for i, num in enumerate(seq):
     if info.get(num) == None:
         info[num] = 0
 
     info[num] += 1
 
-sorted_result = sorted(info.items(), key = lambda x: -x[1]) 
+sorted_result = sorted(info.items(), key=lambda x: -x[1])
 
-result = ''
+result = ""
 for num, freq in sorted_result:
     for _ in range(freq):
-        print(num, end=' ')
+        print(num, end=" ")
