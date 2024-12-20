3970d2e460c2145a9148d84af3389a78da8f1bb0
irene622 <dywjd7272@naver.com>
2024-12-20 18:27:21 +0900

    lint

--- string/1515수이어쓰기.py
+++ string/1515수이어쓰기.py
@@ -5,7 +5,7 @@ number_origin = sys.stdin.readline().strip()
 # 12345678910 -> 2340 -> output = 10
 idx = 0
 while True:
-    # '1' == number_origin[0] -> False. 
+    # '1' == number_origin[0] -> False.
     # '2' == number_origin[0] -> True. '3' == number_origin[1] -> True. '4' == number_origin[2] -> True. ...
     # n = 10. '0' == number_origin[3] -> True. idx += 1 -> idx = 4 -> idx == len(number_origin) -> True.
     for n in range(1, 3001):
