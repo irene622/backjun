22a36e99cb9fd8fbd19a7c8ad3c881db85038d21
irene622 <dywjd7272@naver.com>
2024-12-22 21:52:20 +0900

    f string

---
+++ data_structure/4358생태학.py
@@ -0,0 +1,22 @@
+import sys
+
+sys.stdin = open('data_structure/ex.txt')
+info = {}
+total = 0
+
+while True:
+    name = sys.stdin.readline().strip()
+    if name == "":
+        break
+
+    total += 1
+    if info.get(name) is None:
+        info[name] = 1
+    else:
+        info[name] += 1
+
+
+sorted_info = sorted(info.items(), key = lambda item: item[0])
+
+for k, v in sorted_info:
+    print(f'{k} {v/total*100:.4f}')
