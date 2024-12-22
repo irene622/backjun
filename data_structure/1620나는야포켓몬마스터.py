849b870ab4c07d5fe1ca32a24927a0ccfe8304d6
irene622 <dywjd7272@naver.com>
2024-12-22 21:24:37 +0900

    fix: save two info

--- data_structure/1620나는야포켓몬마스터.py
+++ data_structure/1620나는야포켓몬마스터.py
@@ -2,10 +2,12 @@ import sys
 
 sys.stdin = open('data_structure/ex.txt')
 info_n, ques_n = map(int, sys.stdin.readline().split())
-info = []
+info = {}
 
 for i in range(info_n):
-    info.append(sys.stdin.readline().strip())
+    input =  sys.stdin.readline().strip()
+    info[i] = input
+    info[input] = i    
 
 for i in range(ques_n):
     input = sys.stdin.readline().strip()
@@ -13,4 +15,4 @@ for i in range(ques_n):
         input = int(input)
         print(info[input-1])
     except:
-        print(info.index(input)+1)
+        print(info[input]+1)
