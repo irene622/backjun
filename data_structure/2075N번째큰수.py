57ed6854f36b8236b443f41ba666f214b09af1fa
irene622 <dywjd7272@naver.com>
2024-12-22 22:14:50 +0900

    fix: heap, keep len

--- data_structure/2075N번째큰수.py
+++ data_structure/2075N번째큰수.py
@@ -1,11 +1,19 @@
 import sys
+import heapq
 
 sys.stdin = open('data_structure/ex.txt')
 N = int(sys.stdin.readline().strip())
-numbers = []
+heap = []
 for i in range(N):
-    numbers += map(int, sys.stdin.readline().strip().split())
+    numbers = map(int, sys.stdin.readline().strip().split())
 
-print(sorted(numbers, reverse=True)[N-1])
+    if i == 0:
+        for num in numbers:
+            heapq.heappush(heap, num)
+    else:
+        for num in numbers:
+            if heap[0] < num:
+                heapq.heappop(heap)
+                heapq.heappush(heap, num)
 
-
+print(heap[0])
