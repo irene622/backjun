e05fa46fab828cbd5e3901f7359fa28a45453da4
irene622 <dywjd7272@naver.com>
2024-12-24 18:57:12 +0900

    lint

--- data_structure/2075N번째큰수.py
+++ data_structure/2075N번째큰수.py
@@ -1,15 +1,15 @@
 import sys
 import heapq
 
-sys.stdin = open('data_structure/ex.txt')
+sys.stdin = open("data_structure/ex.txt")
 N = int(sys.stdin.readline().strip())
-heap = [] # 최소힙 정렬
+heap = []  # 최소힙 정렬
 for i in range(N):
     numbers = map(int, sys.stdin.readline().strip().split())
 
     if i == 0:
         for num in numbers:
-            heapq.heappush(heap, num) # num울 heap에 추가.
+            heapq.heappush(heap, num)  # num울 heap에 추가.
     else:
         for num in numbers:
             if heap[0] < num:
