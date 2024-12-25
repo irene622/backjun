a139b276412449920d185ee855589e2cd671edac
irene622 <dywjd7272@naver.com>
2024-12-26 01:51:42 +0900

    lint

--- data_structure/1620나는야포켓몬마스터.py
+++ data_structure/1620나는야포켓몬마스터.py
@@ -1,18 +1,18 @@
 import sys
 
-sys.stdin = open('data_structure/ex.txt')
+sys.stdin = open("data_structure/ex.txt")
 info_n, ques_n = map(int, sys.stdin.readline().split())
 info = {}
 
 for i in range(info_n):
-    input =  sys.stdin.readline().strip()
+    input = sys.stdin.readline().strip()
     info[i] = input
-    info[input] = i    
+    info[input] = i
 
 for i in range(ques_n):
     input = sys.stdin.readline().strip()
     try:
         input = int(input)
-        print(info[input-1])
+        print(info[input - 1])
     except:
-        print(info[input]+1)
+        print(info[input] + 1)
