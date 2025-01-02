e8629f99ac9e7e799b8c1614ed03d896ba91baa4
irene622 <dywjd7272@naver.com>
2025-01-02 23:54:36 +0900

    sorted

--- data_structure/2304창고다각형.py
+++ data_structure/2304창고다각형.py
@@ -2,3 +2,10 @@ import sys
 
 sys.stdin = open('data_structure/ex.txt')
 col = int(sys.stdin.readline().strip())
+info = []
+for i in range(col):
+    idx, hei = map(int, sys.stdin.readline().strip().split(' '))
+    info.append((idx, hei))
+
+info = sorted(info, key=lambda x : x[0])
+print(info)
