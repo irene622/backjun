3837753e3c968c895bfe49b232bbe5f8151fb96b
irene622 <dywjd7272@naver.com>
2025-01-27 21:18:55 +0900

    heap. sort in tuple[0]

--- data_structure/11286절댓값힙.py
+++ data_structure/11286절댓값힙.py
@@ -1,12 +1,18 @@
 import sys
+import heapq
 
 sys.stdin = open('data_structure/ex.txt')
 N = int(sys.stdin.readline().strip())
 
-abs_sort = []
+abs_heap = []
 for _ in range(N):
     num = int(sys.stdin.readline().strip())
+
     if num != 0:
-        abs_sort.append(num)
-    elif num == 0:
-        abs_sort.pop()
+        heapq.heappush(abs_heap, (abs(num), num))
+    else:
+        if len(abs_heap) == 0:
+            print(0)
+        else:
+            pop = heapq.heappop(abs_heap)
+            print(pop[1])
