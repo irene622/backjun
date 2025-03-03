4bb91f8c068960514d1c53d664798d61bc5f79ee
irene622 <dywjd7272@naver.com>
2025-03-03 23:36:38 +0900

    lint;

--- dfsbfs/10026적록색약.py
+++ dfsbfs/10026적록색약.py
@@ -3,21 +3,24 @@ from collections import deque
 
 num = int(sys.stdin.readline().strip())
 RGB_grid = [sys.stdin.readline().strip() for _ in range(num)]
-visited = [[False]*num for _ in range(num)]
+visited = [[False] * num for _ in range(num)]
 num_RGB_region = 0
-direction = [(1,0),(0,1),(-1,0),(0,-1)] # 근접을 사방으로 확인해서 같은 영역인지 확인한다.
+direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]  # 근접을 사방으로 확인해서 같은 영역인지 확인한다.
+
+
 def check_region(grid, x, y):
     visited[x][y] = True
     status = grid[x][y]
-    next = deque([(i,j)])
-    
+    next = deque([(i, j)])
+
     while next:
         x, y = next.popleft()
-        for dx,dy in direction:
-            if 0<= x+dx <= num-1 and 0<= y+dy <= num-1:
-                if not visited[x+dx][y+dy] and grid[x+dx][y+dy] == status:
-                    visited[x+dx][y+dy] = True
-                    next.append((x+dx, y+dy))
+        for dx, dy in direction:
+            if 0 <= x + dx <= num - 1 and 0 <= y + dy <= num - 1:
+                if not visited[x + dx][y + dy] and grid[x + dx][y + dy] == status:
+                    visited[x + dx][y + dy] = True
+                    next.append((x + dx, y + dy))
+
 
 for i in range(num):
     for j in range(num):
@@ -25,8 +28,8 @@ for i in range(num):
             check_region(RGB_grid, i, j)
             num_RGB_region += 1
 
-RB_grid = [line.replace('G', 'R') for line in RGB_grid]
-visited = [[False]*num for _ in range(num)]
+RB_grid = [line.replace("G", "R") for line in RGB_grid]
+visited = [[False] * num for _ in range(num)]
 num_RB_region = 0
 for i in range(num):
     for j in range(num):
